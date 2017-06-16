a33499327562708967ba2eb7878c64764771a67f
Victor Medeiros <victor.medeiros@ifnet-for.ifactory.com.br>
2017-06-16 13:03:26 -0300

    chat almost complete!

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const socket = require('socket.io');
+
+const app = express();
+const server = app.listen(4000, () => console.log('Listening to requests on localhost:4000') );
+
+app.use(express.static('public'));
+
+
+//Socket setup
+const io = socket(server);
+
+io.on('connection', socket => {
+    console.log('Chat socket connected! id: ', socket.id);
+
+    socket.on('chat', data => {
+        io.sockets.emit('chat', data);
+    });
+});
