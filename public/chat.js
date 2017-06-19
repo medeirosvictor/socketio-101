8e184f5ea500f7ee298c7e90a0fab2832112eb6b
Victor Medeiros <victor.medeiros@ifnet-for.ifactory.com.br>
2017-06-19 08:04:25 -0300

    Final commit, chat is working properly maybe i'll want to host it somewhere

--- public/chat.js
+++ public/chat.js
@@ -3,7 +3,8 @@ const socket = io.connect('http://localhost:4000');
 var message = document.getElementById('message');
       handle = document.getElementById('handle'),
       btn = document.getElementById('send'),
-      output = document.getElementById('output');
+      output = document.getElementById('output'),
+      feedback = document.getElementById('feedback');
 
 btn.addEventListener('click', () => {
     socket.emit('chat', {
@@ -12,6 +13,14 @@ btn.addEventListener('click', () => {
     })
 });
 
+message.addEventListener('keypress', () => {
+    socket.emit('typing', handle.value);
+});
+
 socket.on('chat', data => {
+    feedback.innerHTML = "";
     output.innerHTML += "<p><strong>"+ data.handle +": </strong>"+ data.message+"</p>";
+});
+socket.on('typing', data => {
+    feedback.innerHTML = "<p><em>"+data+" is typing a message...</em></p>";
 });
